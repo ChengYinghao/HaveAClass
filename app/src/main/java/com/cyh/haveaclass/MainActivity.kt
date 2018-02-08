add124783ed8b62bbdc8f9f8240a2f5afb69c129
ZhengKeli <zhengkeli2009@126.com>
2018-02-08 23:04:58 +0700

    改包名

--- app/src/main/java/com/cyh/haveaclass/MainActivity.kt
+++ app/src/main/java/com/cyh/haveaclass/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.example.cyh.haveaclass
+package com.cyh.haveaclass
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
