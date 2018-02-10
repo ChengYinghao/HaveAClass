1140ea44ec067662b9e56a49f1140602fc97b479
ChengYinghao <1131637423@qq.com>
2018-02-10 11:14:00 +0700

    实现了目前所要实现功能的接口。

---
+++ app/src/main/java/com/cyh/haveaclass/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cyh.haveaclass;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cyh.haveaclass.R;
+
+/**
+ * Created by CYH on 2018/2/9.
+ */
+
+public class MainActivity extends AppCompatActivity{
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
