1ea221da2f37eeca719b912ed8d3f406f7e094fb
ZhengKeli <zhengkeli2009@126.com>
2018-02-13 23:53:15 +0700

    加了core模块的单元测试

--- core/src/main/java/com/cyh/haveaclass/core/ClassTable.kt
+++ core/src/main/java/com/cyh/haveaclass/core/ClassTable.kt
@@ -5,5 +5,4 @@ interface ClassTable {
 	fun allClasses(): Collection<AClass>
 	fun selectClass(day: Int, section: Int): Collection<AClass>
 	fun addAClass(aClass: AClass)
-	
 }
--- core/src/main/java/com/cyh/haveaclass/core/myClass.java
+++
@@ -1,4 +0,0 @@
-package com.cyh.haveaclass.core;
-
-public class myClass {
-}
