12460071907363c3e8720c46154fecfc43adb641
ChengYinghao <1131637423@qq.com>
2018-04-08 10:53:14 +0700

    初步实现搜索功能。

--- app/src/main/java/com/cyh/haveaclass/ui/SearchLessonFragment.kt
+++ app/src/main/java/com/cyh/haveaclass/ui/SearchLessonFragment.kt
@@ -12,7 +12,6 @@ import com.cyh.haveaclass.R
 import com.cyh.haveaclass.core.Lesson
 import com.cyh.haveaclass.core.PlanUtils
 import com.cyh.haveaclass.core.WebSitePlan
-import kotlinx.android.synthetic.main.fragment_lesson_list.*
 import kotlinx.android.synthetic.main.search_lesson_fragment.*
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
@@ -20,93 +19,91 @@ import java.util.*
 
 
 class SearchLessonFragment : Fragment(), SearchView.OnQueryTextListener {
-	override fun onQueryTextSubmit(query: String?): Boolean {
-		refreshLessonList(query)
-		return false
-	}
-	
-	override fun onQueryTextChange(newText: String?): Boolean {
-		refreshLessonList(newText)
-		return false
-	}
-	
-	companion object {
-		private const val argName_groupName = "groupName"
-		var SearchLessonFragment.argGroupName: String
-			get() = arguments.getString(SearchLessonFragment.argName_groupName)
-			set(value) {
-				arguments = (arguments
-					?: Bundle()).apply { putString(SearchLessonFragment.argName_groupName, value) }
-			}
-		
-		fun newInstance(groupName: String): SearchLessonFragment {
-			return SearchLessonFragment().apply { argGroupName = groupName }
-		}
-	}
-	
-	
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-		return inflater.inflate(R.layout.search_lesson_fragment, container, false)
-	}
-	
-	override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-		
-		searched_lessonListView.adapter = object : BaseAdapter() {
-			override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-				val lessonView = (convertView as? LessonView) ?: LessonView(context)
-				lessonView.lesson = getItem(position)
-				return lessonView
-			}
-			
-			override fun getItem(position: Int): Lesson = search_lessonList[position]
-			
-			override fun getItemId(position: Int): Long = 0L
-			
-			override fun getCount(): Int = search_lessonList.size
-			
-			
-		}
-		search2.setOnQueryTextListener(this)
-	}
-	
-	private var search_lessonList: List<Lesson> = emptyList()
-	
-	private fun refreshLessonList(query: String?) {
-		if (query.equals(null)) {
-			return
-		}
-		swipeRefreshLayout.isRefreshing = true
-		launch {
-			try {
-				webSitePlan.fetchLessons()
-			} catch (e: Exception) {
-				launch(UI) {
-					Toast.makeText(getContext(), "获取课表信息失败！", Toast.LENGTH_SHORT).show()
-					swipeRefreshLayout.isRefreshing = false
-				}
-				return@launch
-			}
-			launch(UI) {
-				val calendar = Calendar.getInstance()
-				val nowSection = PlanUtils.nowSection2(calendar)
-				
-				val allLessons = webSitePlan.allLessons()
-				search_lessonList = allLessons.filter {
-					if (it.section.weekType == nowSection.weekType) {
-						it.name.indexOf(query!!) != 0 || it.teacher.indexOf(query) != 0
-					} else false
-				}
-				(lessonListView.adapter as BaseAdapter).notifyDataSetChanged()
-				swipeRefreshLayout.isRefreshing = false
-			}
-		}
-	}
-	
-	private lateinit var webSitePlan: WebSitePlan
-	
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		webSitePlan = WebSitePlan(argGroupName)
-	}
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        refreshLessonList(query)
+        return false
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        refreshLessonList(newText)
+        return false
+    }
+
+    companion object {
+        private const val argName_groupName = "groupName"
+        var SearchLessonFragment.argGroupName: String
+            get() = arguments.getString(SearchLessonFragment.argName_groupName)
+            set(value) {
+                arguments = (arguments
+                        ?: Bundle()).apply { putString(SearchLessonFragment.argName_groupName, value) }
+            }
+
+        fun newInstance(groupName: String): SearchLessonFragment {
+            return SearchLessonFragment().apply { argGroupName = groupName }
+        }
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.search_lesson_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        searched_lessonListView.adapter = object : BaseAdapter() {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                val lessonView = (convertView as? LessonView) ?: LessonView(context)
+                lessonView.lesson = getItem(position)
+                return lessonView
+            }
+
+            override fun getItem(position: Int): Lesson = search_lessonList[position]
+
+            override fun getItemId(position: Int): Long = 0L
+
+            override fun getCount(): Int = search_lessonList.size
+
+
+        }
+        search2.setOnQueryTextListener(this)
+    }
+
+    private var search_lessonList: List<Lesson> = emptyList()
+
+    private fun refreshLessonList(query: String?) {
+        if (query.equals(null)) {
+            return
+        }
+        search_swipeRefreshLayout.isRefreshing = true
+        launch {
+            try {
+                webSitePlan.fetchLessons()
+            } catch (e: Exception) {
+                launch(UI) {
+                    Toast.makeText(getContext(), "获取课表信息失败！", Toast.LENGTH_SHORT).show()
+                    search_swipeRefreshLayout.isRefreshing = false
+                }
+                return@launch
+            }
+            launch(UI) {
+                val calendar = Calendar.getInstance()
+                val nowSection = PlanUtils.nowSection2(calendar)
+
+                val allLessons = webSitePlan.allLessons()
+                search_lessonList = allLessons.filter {
+                    it.name.toLowerCase().indexOf(query!!.toLowerCase()) != -1 || it.teacher.indexOf(query.toLowerCase()) != -1
+                }
+                (searched_lessonListView.adapter as BaseAdapter).notifyDataSetChanged()
+                search_swipeRefreshLayout.isRefreshing = false
+            }
+        }
+    }
+
+    private lateinit var webSitePlan: WebSitePlan
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        webSitePlan = WebSitePlan(argGroupName)
+    }
 }
