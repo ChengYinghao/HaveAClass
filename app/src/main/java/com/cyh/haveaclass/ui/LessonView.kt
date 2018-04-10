0fae711015bb23032db1b1e46d2a021e3e322979
ChengYinghao <1131637423@qq.com>
2018-04-10 10:55:41 +0700

    增加了日期标签。

--- app/src/main/java/com/cyh/haveaclass/ui/LessonView.kt
+++ app/src/main/java/com/cyh/haveaclass/ui/LessonView.kt
@@ -10,24 +10,28 @@ import com.cyh.haveaclass.core.PlanUtils
 import kotlinx.android.synthetic.main.cv_lesson.view.*
 
 class LessonView @JvmOverloads constructor(
-	context: Context,
-	attrs: AttributeSet? = null,
-	defStyleAttr: Int = 0
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
-	init {
-		LayoutInflater.from(this.context).inflate(R.layout.cv_lesson, this, true)
-	}
-	
-	var lesson: Lesson? = null
-		set(value) {
-			field = value
-			timeLabel.text = PlanUtils.sectionOfDayToText(value?.section?.sectionOfDay ?: -1)
-			placeLabel.text = value?.place ?: ""
-			nameLabel.text = value?.run { "$name $type" } ?: ""
-			teacherLabel.text = value?.teacher ?: ""
-			
-			val color = (value?.name ?: "").hashCode() % 0x888888 + 0x888888
-			mainLayout.setBackgroundColor(color)
-		}
-	
+    init {
+        LayoutInflater.from(this.context).inflate(R.layout.cv_lesson, this, true)
+    }
+
+    var lesson: Lesson? = null
+        set(value) {
+            field = value
+            timeLabel.text = PlanUtils.sectionOfDayToText(value?.section?.sectionOfDay ?: -1)
+            placeLabel.text = value?.place ?: ""
+            nameLabel.text = value?.run {
+                val s = "$name $type"
+                s
+            } ?: ""
+            teacherLabel.text = value?.teacher ?: ""
+            dayLabel.text = PlanUtils.dayOfWeekToText(value?.section?.dayOfWeek ?: -1)
+
+            val color = (value?.name ?: "").hashCode() % 0x888888 + 0x888888
+            mainLayout.setBackgroundColor(color)
+        }
+
 }
