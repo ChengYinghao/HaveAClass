3913fd834f6b7fbd533a6b87e93054c8dfeed68e
ChengYinghao <1131637423@qq.com>
2018-04-14 13:18:39 +0700

    设置部分貌似成功了？

--- app/src/main/java/com/cyh/haveaclass/ui/MainActivity.kt
+++ app/src/main/java/com/cyh/haveaclass/ui/MainActivity.kt
@@ -3,18 +3,25 @@ package com.cyh.haveaclass.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.cyh.haveaclass.R
+import com.cyh.haveaclass.control.MyApp
 
 class MainActivity : AppCompatActivity() {
-	
-	public override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
-		
-		if (savedInstanceState == null) {
-			supportFragmentManager.beginTransaction()
-				.add(R.id.fragmentContainer, LessonListFragment.newInstance("150Б52"))
-				.commit()
-		}
-	}
-	
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val app = applicationContext as MyApp
+        val manager = app.settingsManager
+        if (savedInstanceState == null) {
+            val groupName = manager.settings.groupName
+            if (groupName.equals("")) {
+                supportFragmentManager.beginTransaction()
+                        .add(R.id.fragmentContainer, SettingFragment.newInstance())
+                        .commit()
+            } else supportFragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainer, LessonListFragment.newInstance(groupName))
+                    .commit()
+        }
+    }
+
 }
--- app/src/main/java/com/cyh/haveaclass/ui/SettingFragment.kt
+++ app/src/main/java/com/cyh/haveaclass/ui/SettingFragment.kt
@@ -2,29 +2,42 @@ package com.cyh.haveaclass.ui
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.cyh.haveaclass.R
+import com.cyh.haveaclass.control.MyApp
 import kotlinx.android.synthetic.main.settings.*
 
 
-/**
- * Created by CYH on 2018/4/13.
- */
+class SettingFragment : Fragment() {
+    companion object {
+        fun newInstance(): SettingFragment {
+            return SettingFragment()
+        }
+    }
 
-class SettingFragment: Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.settings, container, false)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        set_determine_button.setOnClickListener{
+        val myApp = context.applicationContext as MyApp
+        val manager = myApp.settingsManager
+        val builder = manager.settings.toBuilder()
+        set_determine_button.setOnClickListener {
             // 获取用户输入的组号
-            val groupName: Editable? = team_editText.text
+            val groupName: String? = team_editText.text.toString()
+            if (groupName != null) {
+                val vary_builder = builder.setGroupName(groupName)
+                manager.saveSettings(vary_builder.buildPartial())
+            }
+
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, LessonListFragment.newInstance(manager.loadSettings()!!.groupName))
+                    .addToBackStack(null)
+                    .commit()
         }
     }
 }
